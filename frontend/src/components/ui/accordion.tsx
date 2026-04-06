aa5114a4c7f83a16d68e8a10b6afdecf7093518a
carlton-source <carlton.blessing.paul@gmail.com>
2026-04-06 11:22:48 +0100

    Fix AccordionContent component by adding missing newline at the end of the file

--- frontend/src/components/ui/accordion.tsx
+++ frontend/src/components/ui/accordion.tsx
@@ -48,3 +48,5 @@ const AccordionContent = React.forwardRef<
 ));
 
 AccordionContent.displayName = AccordionPrimitive.Content.displayName;
+
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent };
