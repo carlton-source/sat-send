4423b8683f8397bdbe5668f539eaff741083dcaf
carlton-source <carlton.blessing.paul@gmail.com>
2026-04-06 10:50:12 +0100

    Add AppShell component with initial imports and structure

---
+++ frontend/src/components/layout/AppShell.tsx
@@ -0,0 +1,4 @@
+import { Link, useLocation } from "react-router-dom";
+import { LayoutDashboard, Send, Compass, Clock, Trophy } from "lucide-react";
+import { Navbar } from "./Navbar";
+import { cn } from "@/lib/utils";
