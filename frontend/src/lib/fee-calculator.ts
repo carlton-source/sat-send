70668d05363c998e925cfc0a24edd11a6a3a3aa2
carlton-source <carlton.blessing.paul@gmail.com>
2026-04-08 17:14:24 +0100

    Add FeeBreakdown interface for structured fee data representation

--- frontend/src/lib/fee-calculator.ts
+++ frontend/src/lib/fee-calculator.ts
@@ -1,1 +1,11 @@
 import { PLATFORM_FEE_BPS, MICRO_STX_PER_STX } from "./constants";
+
+export interface FeeBreakdown {
+  grossAmount: number;
+  feeAmount: number;
+  netAmount: number;
+  feeBps: number;
+  grossMicroStx: number;
+  feeMicroStx: number;
+  netMicroStx: number;
+}
