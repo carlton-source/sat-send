6f956071cf289694b0ca4a9c2116cb38e0299944
carlton-source <carlton.blessing.paul@gmail.com>
2026-05-12 15:25:54 +0100

    Add closing Card and TabsContent tags in User Lookup tab

--- frontend/src/pages/Explore.tsx
+++ frontend/src/pages/Explore.tsx
@@ -242,3 +242,6 @@ export default function Explore() {
                       </div>
                     </div>
                   )}
+                  </CardContent>
+              </Card>
+            </TabsContent>
